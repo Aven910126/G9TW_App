d999cef72800758edf934227b95aa225685d2ce5
Aven910126 <a1148370488@gmail.com>
2022-05-19 02:41:11 +0800

    Api create

--- app/src/main/java/com/example/tw2ver01/page_emergency_input.java
+++ app/src/main/java/com/example/tw2ver01/page_emergency_input.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
@@ -18,6 +19,13 @@ import android.view.View;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import java.io.IOException;
 import java.text.BreakIterator;
 import java.text.DecimalFormat;
@@ -27,40 +35,82 @@ import org.json.JSONObject;
 import java.io.IOException;
 //import okhttp3.MediaType;
 
-
-
-
 public class page_emergency_input extends AppCompatActivity {
     public Button btnConfirm,btnBackmain;
     public static final String EXTRA_MESSAGE="com.example.tw2ver01.MESSAGE";
     public static final String EXTRA_MESSAGE1="com.example.tw2ver01.MESSAGE";
     public static final String EXTRA_MESSAGE2="com.example.tw2ver01.MESSAGE";
-
+    OkHttpClient client = new OkHttpClient();
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_page_emergency_input);
-
-    }
-    public  void gotoemergencyoutcome(View view){
-
-        Intent intent = new  Intent(page_emergency_input.this, page_emergency_outcome.class);
-        Intent intent1 = new  Intent(page_emergency_input.this, page_emergency_outcome.class);
-        Intent intent2 = new  Intent(page_emergency_input.this, page_emergency_outcome.class);
-        Bundle bundle = new Bundle();
-        EditText name = (EditText) findViewById(R.id.name);//取得聯絡人姓名
-        EditText call = (EditText) findViewById(R.id.call);//取得聯絡人稱謂
-        EditText phone = (EditText) findViewById(R.id.phone);//取得聯絡人電話
-        String name1 = name.getText().toString();
-        String call1 = call.getText().toString();
-        String phone1 = phone.getText().toString();
-        bundle.putString("name",name1);
-        bundle.putString("call", call1);
-        bundle.putString("phone",phone1);
-        intent.putExtras(bundle);
-        startActivity(intent);
+        btnConfirm = findViewById(R.id.button);
+//        btnBackmain.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new  Intent(page_emergency_input.this, page_emergency_outcome.class);
+//                startActivity(intent);
+//            }
+//        });
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JSONObject jsonObject = new JSONObject();
+                Bundle bundle = new Bundle();
+                EditText name = (EditText) findViewById(R.id.name);
+                EditText re = (EditText) findViewById(R.id.call);
+                EditText phone = (EditText) findViewById(R.id.phone);
+                String devicecode = "0122";
+                String name1 = name.getText().toString();
+                String call1 = re.getText().toString();
+                String phone1 = phone.getText().toString();
+                bundle.putString("name",name1);
+                bundle.putString("call", call1);
+                bundle.putString("phone",phone1);
+                try{
+                    jsonObject.put("contactNo", phone.getText().toString());
+                    jsonObject.put("contactPerson", name.getText().toString());
+                    jsonObject.put("deviceCode", "0122");
+                    jsonObject.put("relationship", re.getText().toString());
+                    System.out.println(jsonObject);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+                RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
+                Request request = new Request.Builder()
+                        .url("http://127.0.0.1:8080/api/EmergencyContact/create")
+                        .post(body)
+                        .build();
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        // 連線成功
+                        String result = response.body().string();
+                        Log.d("OkHttp result", result);
+                    }
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println(request);
+                    }
+                });
+//
+//                try{
+//                    Response response = client.newCall(request).execute();
+//                    if(response.isSuccessful()){
+//                        System.out.println(response);
+//                    }
+//                } catch (IOException e) {
+//                    System.out.println(request);
+//                }
+                Intent intent = new  Intent(page_emergency_input.this, page_emergency_outcome.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 //      btnConfirm.setOnClickListener(new View.OnClickListener(){
 //        @Override
@@ -110,4 +160,4 @@ public class page_emergency_input extends AppCompatActivity {
 //        public void goback (View v){
 //        finish();
 //        }
-    }
+}
