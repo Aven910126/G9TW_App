1986e1a88f9d8650d1c4c7a7e5754d9740e20b6c
Tim <ricay223@gmail.com>
2022-05-28 03:56:08 +0800

    again

--- app/src/main/java/com/example/tw2ver01/UserSevice.java
+++ app/src/main/java/com/example/tw2ver01/UserSevice.java
@@ -1,15 +1,13 @@
 package com.example.tw2ver01;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+
 
 public interface UserSevice {
     @POST("account/login/")
     Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
 
-    @POST("account/create/")
+    @POST("EmergencyContact/create/")
     Call<RegisterResponse> registerUser(@Body RegisterRequest registerRequset);
-
-
 }
